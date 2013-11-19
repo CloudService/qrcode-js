fea632a0f90a8faf21c74eabca8fc472eed0a7f5
Jeffrey Sun <jeffrey.zksun@gmail.com>
2013-11-19 15:18:37 +0800

    expose more properties

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 var qrcode = require('./lib/qrcode.js');
 
 module.exports = {
+    typeNumber: 4,
+    errorCorrectLevel: 'L',
     toBase64: function(text, size){
-        var qr = qrcode(4, 'M');
+        var qr = qrcode(this.typeNumber, this.errorCorrectLevel);
         qr.addData(text);
         qr.make();
         var base64 = qr.createImgBase64(size);
